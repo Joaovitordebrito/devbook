fffbb3a1413d48977aba1660ff4ab46b4596ac21
Joao Brito <jvbrito12@hotmail.com>
2023-10-04 15:16:58 -0300

    chore: innital config

---
+++ api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"api/src/config"
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.LoadConfig()
+
+	fmt.Printf("Api running on port %d", config.Port)
+
+	r := router.Generate()
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
+}
